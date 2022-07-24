c522bb7ccc77e5c56008ebf64411ee162b6d5de8
´Peco´ <pecohernandez6@gmail.com>
2022-07-24 11:05:21 -0300

    Realice correcciones en el punto 2

--- punto2.js
+++ punto2.js
@@ -2,11 +2,11 @@
 let ciudades2 = [];
 
 do {
-  let Ciudades = prompt("Introduce una cadena");
+  let Ciudades = prompt("Introduce una ciudad");
   ciudades2.push (Ciudades)
 } while (confirm("Desea seguir?"));
 
-document.write(ciudades2);
+document.write('<br>'+ciudades2);
 //mostrar longuitud de arreglo
 console.log (ciudades2.length)
 //Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
@@ -15,7 +15,9 @@ console.log (ciudades2[3])
 console.log (ciudades2[ciudades2.length-1])
 //Añade en última posición la ciudad de París.
 ciudades2.push("Paris")
+document.write('<br>'+ciudades2);
 //Escribe por pantalla el elemento que ocupa la segunda posición.
-document.write (ciudades2[1])
+document.write ('<br>'+ciudades2[1])
 //Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
 ciudades2[1] = 'Barcelona';
+document.write('<br>'+ciudades2);
